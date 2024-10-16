ef18606407d331993943f528c0b3c10a8d6a6ad5
xyw <xyw283@gmail.com>
2024-10-16 14:22:47 +0800

    integrated gateway

--- service-provider-dubbo/src/main/java/com/main/provider/impl/StringInterfaceImpl.java
+++ service-provider-dubbo/src/main/java/com/main/provider/impl/StringInterfaceImpl.java
@@ -3,7 +3,7 @@ package com.main.provider.impl;
 import com.main.rpc.StringInterface;
 import org.apache.dubbo.config.annotation.DubboService;
 
-@DubboService(registry = "service-provider-dubbo")
+@DubboService(registry = "service-provider-dubbo-8002")
 public class StringInterfaceImpl implements StringInterface {
   @Override
   public String getData() {
